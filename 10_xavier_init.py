81d78d3ea269c6431828807ba9bf3bb23c05a4cd
jng0820@knu.ac.kr <dbstjr12>
2018-07-02 13:29:51 +0900

    mnist_xavier commit

---
+++ 10_xavier_init.py
@@ -0,0 +1,61 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import random
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32,[None, 10])
+
+dropout_rate = tf.placeholder(tf.float32)
+
+W1 = tf.get_variable("W1",shape = [784,512], initializer= tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([512]))
+L1 = tf.nn.relu(tf.matmul(X,W1)+b1)
+
+W2 = tf.get_variable("W2",shape = [512,512], initializer= tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([512]))
+L2 = tf.nn.relu(tf.matmul(L1,W2)+b2)
+
+
+W3 = tf.get_variable("W3",shape = [512,512], initializer= tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([512]))
+L3 = tf.nn.relu(tf.matmul(L2,W3)+b3)
+
+
+W4 = tf.get_variable("W4",shape = [512,512], initializer= tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([512]))
+L4 = tf.nn.relu(tf.matmul(L3,W4)+b4)
+
+
+W5 = tf.get_variable("W5",shape = [512,10], initializer= tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.add(tf.matmul(L4,W5),b5)
+
+learning_rate = 0.01
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+training_epochs = 15
+batch_size = 100
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        c, _ = sess.run([cost,optimizer],feed_dict={X:batch_xs,Y:batch_ys,dropout_rate : 0.7})
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch+1), 'cost =','{:.9f}'.format(avg_cost))
+
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: mnist.test.images, Y: mnist.test.labels, dropout_rate : 1}))
+
