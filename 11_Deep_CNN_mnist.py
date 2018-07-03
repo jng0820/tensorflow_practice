a5abb9d3e63f232165d2e99e15e7dd09e85f93b8
jng0820@knu.ac.kr <dbstjr12>
2018-07-03 13:02:13 +0900

    deep_cnn_mnist commit

--- 11_Deep_CNN_mnist.py
+++ 11_Deep_CNN_mnist.py
@@ -6,39 +6,39 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-keep_prop = tf.Variable(tf.float32)
+keep_prob = tf.placeholder("float")
 
 X = tf.placeholder(tf.float32,shape=[None,784])
 X_img = tf.reshape(X,[-1,28,28,1])
 Y = tf.placeholder(tf.float32,shape=[None,10])
 
 
-W1 = tf.Variable(tf.random_normal([3,3,1,32],stddev=0.01))
-L1 = tf.nn.conv2d(X_img,W1,strides=[1,1,1,1],padding = 'SAME')
+W1 = tf.Variable(tf.random_normal([5,5,1,32],stddev=0.01))
+L1 = tf.nn.conv2d(X_img,W1,strides=[1,1,1,1])
 L1 = tf.nn.relu(L1)
-L1 = tf.nn.max_pool(L1, ksize=[1,2,2,1],strides=[1,2,2,1], padding='SAME')
-L1 = tf.nn.dropout(L1,keep_prob=keep_prop)
+L1 = tf.nn.max_pool(L1, ksize=[1,2,2,1],strides=[1,1,1,1])
+L1 = tf.nn.dropout(L1,keep_prob=keep_prob)
 
 W2 = tf.Variable(tf.random_normal([3,3,32,64],stddev=0.01))
 
 L2 = tf.nn.conv2d(L1,W2,strides=[1,1,1,1],padding = 'SAME')
 L2 = tf.nn.relu(L2)
 L2 = tf.nn.max_pool(L2, ksize=[1,2,2,1],strides=[1,2,2,1], padding='SAME')
-L2 = tf.nn.dropout(L2,keep_prob=keep_prop)
+L2 = tf.nn.dropout(L2,keep_prob=keep_prob)
 
 W3 = tf.Variable(tf.random_normal([3,3,64,128],stddev=0.01))
 
 L3 = tf.nn.conv2d(L2,W3,strides=[1,1,1,1],padding = 'SAME')
 L3 = tf.nn.relu(L3)
 L3 = tf.nn.max_pool(L3, ksize=[1,2,2,1],strides=[1,2,2,1], padding='SAME')
-L3 = tf.nn.dropout(L3,keep_prob=keep_prop)
+L3 = tf.nn.dropout(L3,keep_prob=keep_prob)
 L3 = tf.reshape(L3,[-1,128*4*4])
 
 
 W4 = tf.get_variable("W4",shape=[128*4*4,625],initializer=tf.contrib.layers.xavier_initializer())
 b = tf.Variable(tf.random_normal([625]))
 L4 = tf.nn.relu(tf.matmul(L3,W4)+b)
-L4 = tf.nn.dropout(L4,keep_prop=keep_prop)
+L4 = tf.nn.dropout(L4,keep_prop=keep_prob)
 
 W5 = tf.get_variable("W5",shape=[625,10],initializer=tf.conrib.layers.xavier_initializer())
 b2 = tf.Varaible(tf.random_normal([10]))
@@ -58,11 +58,11 @@ for epoch in range(training_epoch):
     total_batch = int(mnist.train.num_examples / batch_size)
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        c, _, = sess.run([cost,optimizer],feed_dict={X:batch_xs, Y:batch_ys,keep_prop : 0.75})
+        c, _, = sess.run([cost,optimizer],feed_dict={X:batch_xs, Y:batch_ys,keep_prob : 0.75})
         avg_cost += c/ total_batch
 
     print("Epoch : ", '%04d' % (epoch +1), 'cost = ','{:.9f}'.format(avg_cost))
 
 correct_prediction = tf.equal(tf.argmax(hypothesis,1),tf.argmax(Y,1))
 accruacy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print('Accuracy : ', sess.run(accruacy,feed_dict={X:mnist.test.images, Y:mnist.test.labels,keep_prop : 1}))
+print('Accuracy : ', sess.run(accruacy,feed_dict={X:mnist.test.images, Y:mnist.test.labels,keep_prob : 1}))
